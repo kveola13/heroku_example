1b65474605a282c2059239fbf2ba284e501d4f5c
Ola Kveli <olakveli@yahoo.com>
2018-10-01 09:49:01 +0200

    init

---
+++ terraform/heroku_app.tf
@@ -0,0 +1,9 @@
+provider "heroku" {
+  email   = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+# Create a new application
+resource "heroku_app" "myapp" {
+  name = "${var.app_name}"
+}
